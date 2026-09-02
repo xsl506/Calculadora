99e8b0c4d26e3ae979e9ac92964c972577bf6126
xio <xiomarasaenzloria@gmail.com>
2026-09-01 18:59:34 -0600

    cambie los parametros otra vez porque si

--- main.py
+++ main.py
@@ -4,18 +4,18 @@
 
 # AREA DE ESCRITURA PARA XIOMARA
 
-def sumar (numero_uno, numero_dos):
-    return numero_uno + numero_dos
+def sumar (a, b):
+    return a + b
 
-def restar (numero_uno, numero_dos):
-   return numero_uno - numero_dos
+def restar (a, b):
+   return a - b
 
 #------------------------------------
 
 # AREA DE ESCRITURA PARA GINO
 
-def multiplicar(a, b):
-    return a * b
+def multiplicar(numero_uno, numero_dos):
+    return numero_uno * numero_dos
 
-def dividir(a, b):
-    return a / b
+def dividir(numero_uno, numero_dos):
+    return numero_uno / numero_dos
