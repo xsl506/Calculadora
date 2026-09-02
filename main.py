17ffc814fc342b1603684a4c99948ff0380fa39c
Xiomara Saenz Loria <xiomarasaenzloria@MacBook-Air-de-Xiomara.local>
2026-09-01 18:35:28 -0600

    Creando las funciones sumar y restar

--- main.py
+++ main.py
@@ -4,6 +4,12 @@
 
 # AREA DE ESCRITURA PARA XIOMARA
 
+def sumar (numero_uno, numero_dos):
+    return numero_uno + numero_dos
+
+def restar (primer_numero, segundo_numero):
+   return primer_numero - segundo_numero
+
 #------------------------------------
 
 # AREA DE ESCRITURA PARA GINO
